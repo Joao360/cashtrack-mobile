a06c80ddcb41897717dd6fbb33008aa73e321371
João Graça <41423@alunos.isel.ipl.pt>
2019-05-19 13:29:46 +0100

    Change 2 columns to 1

--- components/RecordList.js
+++ components/RecordList.js
@@ -1,45 +1,25 @@
 import React from 'react'
-import { View, FlatList, StyleSheet, TouchableHighlight } from 'react-native'
+import { FlatList, StyleSheet, TouchableHighlight } from 'react-native'
 import PropTypes from 'prop-types'
 
 import RecordType from '../propTypes/RecordType'
 import Record from './Record'
 
-const numOfColumns = 2
-
 const RecordList = ({ records, onRecordPress }) => {
   const renderItem = ({ item }) => (
-    item.invisible
-      ? <View style={styles.item} />
-      : (
-        <TouchableHighlight style={styles.item} onPress={() => onRecordPress(item)}>
-          <Record record={item} />
-        </TouchableHighlight>
-      )
+    <TouchableHighlight style={styles.item} onPress={() => onRecordPress(item)}>
+      <Record record={item} />
+    </TouchableHighlight>
   )
 
   return (
     <FlatList
       style={styles.list}
-      data={formatData(records, numOfColumns)}
+      data={records}
       renderItem={renderItem}
-      numColumns={numOfColumns}
       keyExtractor={(item) => item.id}
     />
   )
-}
-
-const formatData = (data, numOfColumns) => {
-  const newData = [...data]
-  let numberOfElementsLastRow = data.length % numOfColumns
-  while (numberOfElementsLastRow !== 0 && numberOfElementsLastRow !== numOfColumns) {
-    newData.push({
-      invisible: true
-    })
-    numberOfElementsLastRow++
-  }
-
-  return newData
 }
 
 RecordList.propTypes = {
@@ -49,10 +29,6 @@ RecordList.propTypes = {
 }
 
 const styles = StyleSheet.create({
-  hidden: {
-    backgroundColor: 'transparent',
-    elevation: 0
-  },
   item: {
     flex: 1
   },
--- containers/RecordEdition.js
+++ containers/RecordEdition.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, TextInput, Button, StyleSheet, Picker, Text, ScrollView } from 'react-native'
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native'
 import { ButtonGroup } from 'react-native-elements'
 import { connect } from 'react-redux'
 
